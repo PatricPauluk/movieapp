4dc276b77a9fcc5845b9b06ec23e7203049ca95d
Patric Pauluk <patric.pauluk@outlook.com>
2022-12-19 22:52:00 -0300

    finished favorite list

--- src/components/functions/ShowFavoritesButton.jsx
+++ src/components/functions/ShowFavoritesButton.jsx
@@ -15,14 +15,15 @@ const ShowFavoritesButton = ({ setMovies, myFavoritesID }) => {
   const showFavorites = () => {
     favoriteList = [];
 
-    // Bom essa parte aqui merece uma nota bem destacada:
-    // Levei uns 3 dias pra fazer funcionar corretamente.
-    // Tentei criar uma lista através de um useState de array e também não dava certo,
-    // a melhor forma foi essa, alimentando um array comum dentro dele mesmo, e zerando ele
-    // todas as vezes em que foi chamado.
-    // Mas a cereja do bolo esta aqui: o TRY/CATCH TEM DE ESTAR DENTRO DO MAP, e não o map dentro
-    // do try/catch! Acaba não retornando nada, ou retorna as vezes e bugado.
-    // Realmente fiz essa nota ENORME pois levei uns 3 dias pra descobrir.
+    /* Bom essa parte aqui merece uma nota bem destacada:
+    Levei uns 3 dias pra fazer funcionar corretamente.
+    Tentei criar uma lista através de um useState de array e também não dava certo,
+    a melhor forma foi essa, alimentando um array comum dentro dele mesmo, e zerando ele
+    todas as vezes em que foi chamado.
+    Mas a cereja do bolo esta aqui: o TRY/CATCH TEM DE ESTAR DENTRO DO MAP, e não o map dentro
+    do try/catch! Acaba não retornando nada, ou retorna as vezes e bugado.
+    Realmente fiz essa nota ENORME pois levei uns 3 dias pra descobrir.
+    */
     myFavoritesID.map(async id => {
       try {
         const res = await api.get("?i="+id+"&apikey=42dbe614");
